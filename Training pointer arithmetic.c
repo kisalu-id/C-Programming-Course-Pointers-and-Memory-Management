af945f400ccbf1f430e6c632f44cc4145a19d773
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-01-23 13:39:24 +0100

    Update Training pointer arithmetic.c

--- Training pointer arithmetic.c
+++ Training pointer arithmetic.c
@@ -1,6 +1,7 @@
+//edit values in an array using dofferent methods with pointers
 #include <stdio.h>
 
-void swapFifth(int *, int *);
+void swapFifth(int *);
 
 int main(void) {
     //! showMemory(start=65520)
@@ -16,23 +17,25 @@ int main(void) {
     *ptrA = 1;
     *(ptrA + 1) = 2; 
     
-    *arr = 10; //arr has the address of the 1st element of the array => it modifies it
-    *(arr + 1) = 19;
-    ptrA++; //current address ++
-    *ptrA = 20;
+    *arr = 10; //arr has the address of the [0] element of the array => it modifies it
+    *(arr + 1) = 20; //arr[1]
     
-    ptrA += 1;
-    *ptrA = 30;
+    ptrA += 2;  //(arr[1]) +2
+    *ptrA = (arr[1]) + 10;
     
-    arr[4] = (ptrA[3]) + 10;
+    ptrA++; //current address (arr[2]) ++
+    *ptrA = *ptrB * 4;
     
-    swapFifth (&arr[4], &arr[5]);
+    arr[4] = (ptrA[-1] + 20);
+    
+    swapFifth (&arr[5]);
     
     
     printf("Array updated: %d, %d, %d, %d, %d, %d\n", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
     
     return 0;
 }
-void swapFifth (int * a, int * b) {
-    * b = * a;
+
+void swapFifth (int * a) {
+    *a = *(a - 5) * 6;
 }
