1f3568d2470c4e880bdc3f54de9bb78976c0d140
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-01-28 21:37:26 +0100

    Update Allocate and free memory in heap (average grade).c

--- Allocate and free memory in heap (average grade).c
+++ Allocate and free memory in heap (average grade).c
@@ -2,21 +2,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int * allocArr(int);
+double count (int *, int);
+
 int main(void) {
-    //! showMemory(start=272)
     int num;
     double result;
+    int *grades;
   
     printf("How much grades do you want to process?\n");
     scanf("%d", &num);
-    int *grades[num];
+    grades = allocArr(num); //array needs to know it's size in C
   
     printf("Please enter your grades:\n");
     for (int i=0; i<num; i++) {
-        scanf("%d", &(grades + i));
+        scanf("%d", grades + i);
     }
+    result = count(grades, num);
   
     printf("The average grade is: %lf.\n", result);
-    free(result);
+    free(grades); //free the memory in heap from this array
     return 0;
+}
+
+int * allocArr(int a) {
+    int * arr = (int*) malloc(a * sizeof(int)); //get the beginning of this array space, that was allocated to the memory, as arr. not forget to cast to a pointer to an int
+    return arr;
+}
+
+double count (int * grades, int n) {
+    double result = 0;
+    for (int i=0; i<n; i++) {
+        result += grades[i];
+    }
+    result /= n;
+    return result;
 }
